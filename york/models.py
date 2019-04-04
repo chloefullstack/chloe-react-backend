00a9110d7947274c58524baf986b0f21980e031f
chloe xu <choeqxu44@gmail.com>
2019-04-04 13:37:47 -0400

    evaluate the age in django backend

--- york/models.py
+++ york/models.py
@@ -5,9 +5,14 @@ class PuppyInfo(models.Model):
     profile_image = models.ImageField(default='static/images/default.jpg', upload_to='static/images')
     name = models.CharField(max_length=20, null=True, blank=True)
     sex = models.CharField(max_length=6, null=True, blank=True)
-    age = models.CharField(max_length=15, null=True, blank=True)
+    age = models.CharField(max_length=2, null=True, blank=True)
     description = models.TextField(max_length=2000, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+
+    
+    def save(self, *args, **kwargs):
+        self.age = int(self.age) * 10
+        super(PuppyInfo,self).save(*args, **kwargs)
