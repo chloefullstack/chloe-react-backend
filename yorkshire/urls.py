d194a7b31b93e27fec547caac6f5881ac03b53bb
patrick.J <vjiangcg@gmail.com>
2019-04-05 10:03:03 -0400

    new people app

--- yorkshire/urls.py
+++ yorkshire/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path('york/', include('york.urls')),
-
+    # path('people/', include('people.urls')),
 ]
